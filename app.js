51d0d8c390005f56e93cf43c7205bf1419aaba86
Daniel Bonfim <158781988+danielbonfim23@users.noreply.github.com>
2024-07-06 17:57:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+let jogadores = [];
+let palavras = [];
+let palavraParaJogadores = [];
+
+const endpointDaAPI = 'dicionario.json';
+getBuscarPalavras ();
+
+async function getBuscarPalavras() {
+    const res = await fetch(endpointDaAPI);
+    palavras = await res.json();
+   
+}
+
+
+function inserir() {
+    let nomeJogador = document.querySelector('input').value;
+    
+    let divPai = document.querySelector('.jogadores');
+    let divFilho = document.createElement('p');
+    divFilho.classList.add('filhos')
+    divFilho.innerHTML = `<p>${nomeJogador}</p><button onclick="excluir(this)" value="${nomeJogador}">Excluir</button><button onclick="mostrar(this)" value="${nomeJogador}">Mostrar</button>`
+    divPai.appendChild(divFilho);
+    jogadores.push(nomeJogador);
+    // console.log(jogadores)
+}
+
+function excluir(element) {
+    let nomeJogador = element.value;
+    let divPai = document.querySelector('.jogadores');
+    divPai.removeChild(element.parentElement);
+    let index = jogadores.findIndex((element) => element === nomeJogador);
+    if (index !== -1) {
+        jogadores.splice(index, 1);
+    }
+    
+}
+
+function jogar() {
+    if (jogadores.length > 0) {
+        jogadores.forEach(element => {
+            let numero = Math.floor(Math.random() * (palavras.length - 0 + 1) + 0);
+            palavraParaJogadores.push(palavras[numero]);
+    }
+)}
+}
+
+function mostrar(element) {
+    let nomeJogador = element.value;
+    let index = jogadores.findIndex((element) => element === nomeJogador);
+    console.log(jogadores[index])
+
+    let palavraNaTela = document.querySelector('h2');
+    palavraNaTela.innerHTML = palavraParaJogadores[index].palavra;
+}
+
+function esconder() {
+    let palavraNaTela = document.querySelector('h2');
+    palavraNaTela.innerHTML = '';
+}
